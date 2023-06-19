52c851cab3e7ea9bbe21c29a0a257ba008047d3a
Adem Novruzlu <130730588+Ademx1@users.noreply.github.com>
2023-06-19 12:10:04 +0400

    First commit

---
+++ assets/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Shiny manly suit",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "img/Shemi.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Buttoned Mafia Boss shirt",
+    price: 360,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "img/Ədalət_Şükürov.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Goodlooking cream gentlemen suit",
+    price: 680,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "img/eyyubmellim.jpeg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Giorgio Armani blue suit",
+    price: 780,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "img/Shirin.png",
+  },
+];
